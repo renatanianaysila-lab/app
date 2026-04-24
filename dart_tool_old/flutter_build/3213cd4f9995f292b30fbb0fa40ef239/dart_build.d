bbb99e73804047a88c6dbb0b999f5d80ee14c9cf
Naysila Renatania <renatanianaysila@gmail.com>
2026-04-24 20:50:38 +0700

    update

---
+++ dart_tool_old/flutter_build/3213cd4f9995f292b30fbb0fa40ef239/dart_build.d
@@ -0,0 +1,1 @@
+ C:\\Users\\Naysila\ Renatania\\Downloads\\PMOB\ Prakt\\project\\app\\.dart_tool\\flutter_build\\3213cd4f9995f292b30fbb0fa40ef239\\dart_build_result.json: 
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:app/routes/index.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
+  runApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.splash,
-      onGenerateRoute: AppRoutes.generateRoute,
-    );
-  }
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Center(
+          child: Text(
+            'TEST BERHASIL',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
